33a88eb962e4fc2e0a323213a2ff312dcab90f9f
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-27 19:31:59 +0100

    Keyed array child optimization

--- src/jsx-element.ts
+++ src/jsx-element.ts
@@ -6,8 +6,8 @@ export type JsxComponentType<P> = new (props: P) => Component<P>
 export type JsxElementType<P> = string | JsxComponentType<P>
 
 export class JsxElement<P> {
-    root?: HTMLElement
-    element?: HTMLElement
+    root?: Element
+    element?: Element
     component?: Component<P>
     componentElement?: JsxElement<any>
     keyMap: Map<any, JsxElement<any>> = new Map()
@@ -31,7 +31,7 @@ export class JsxElement<P> {
         return 'children' in ps ? <JsxElement<any>[]>ps.children : []
     }
 
-    render(root: HTMLElement): void {
+    render(root: Element): void {
         this.root = root
         if (typeof this.type === 'string') {
             this.renderIntrinsic()
@@ -62,6 +62,10 @@ export class JsxElement<P> {
             }
         }
 
+        if (this.key !== undefined) {
+            this.setAttribute('key', this.key)
+        }
+
         this.root!.appendChild(this.element)
     }
 
@@ -79,24 +83,17 @@ export class JsxElement<P> {
 
     private renderChild(c: any): void {
         if (Array.isArray(c)) {
-            if (c.length === 0 || (c[0] instanceof JsxElement && c[0].key !== undefined)) {
-                const es = c
+            const keyMap = new Map(
+                c
                     .filter(e => e instanceof JsxElement)
                     .map(e => <JsxElement<any>>e)
                     .filter(e => e.key !== undefined)
-                if (es.length === c.length) {
-                    {
-                        // do some smart things instead of this
-                        this.deleteChildren()
-                        c.forEach(sc => this.renderChild(sc))
-                    }
-                    this.keyMap.clear()
-                    es.forEach(e => this.keyMap.set(e.key!, e))
-                }
+                    .map(e => <const>[e.key, e])
+            )
+            if (this.keyMap.size === this.element!.children.length) {
+                this.renderKeyed(keyMap)
             } else {
-                this.keyMap.clear()
-                this.deleteChildren()
-                c.forEach(sc => this.renderChild(sc))
+                this.renderNonKeyed(c, keyMap)
             }
         } else if (c instanceof Signal) {
             this.renderChild(c.get())
@@ -111,6 +108,65 @@ export class JsxElement<P> {
         }
     }
 
+    private renderKeyed(keyMap: Map<any, JsxElement<any>>): void {
+        this.keyMap.forEach((v, k) => {
+            if (!keyMap.has(k)) {
+                v.element?.remove()
+                this.keyMap.delete(k)
+            }
+        })
+        const oldIdxMap = new Map([...this.keyMap.keys()].map((k, i) => [k, i]))
+        const newIdxMap = new Map([...keyMap.keys()].map((k, i) => [k, i]))
+        let lastEl: Element | null = this.element!.firstElementChild
+        newIdxMap.forEach((i, k) => {
+            const e = keyMap.get(k)!
+            const old = this.keyMap.get(k)
+            const oi = oldIdxMap.get(k)
+            if (old && oi !== undefined && oi === i) {
+                e.element = old.element
+                lastEl = old!.element!
+            } else {
+                if (old && old.element) {
+                    e.element = old.element
+                    this.element!.insertBefore(e.element, lastEl?.nextElementSibling ?? null)
+                    lastEl = e.element!
+                } else {
+                    e.render(this.element!)
+                }
+            }
+        })
+        this.keyMap.clear()
+        keyMap.forEach((v, k) => this.keyMap.set(k, v))
+    }
+
+    private renderNonKeyed(c: any[], keyMap: Map<any, JsxElement<any>>): void {
+        const newKeysStr = new Set([...keyMap.keys()].map(k => k.toString()))
+        for (let i = 0; i < this.element!.children.length - 1;) {
+            const e = this.element!.children[i]
+            const k = e.getAttribute('key')
+            if (!k || !newKeysStr.has(k)) {
+                e.remove()
+            } else {
+                i++
+            }
+        }
+        for (const e of c) {
+            if (e instanceof JsxElement && e.key !== undefined) {
+                const old = this.keyMap.get(e.key)
+                if (old && old.element) {
+                    e.element = old.element
+                    // make child last
+                    this.element!.append(e.element)
+                } else {
+                    e.render(this.element!)
+                }
+                this.keyMap.set(e.key, e)
+            } else {
+                this.renderChild(e)
+            }
+        }
+    }
+
     private setAttribute(prop: string, value: any): void {
         let v = value
         if (value instanceof Signal) {
@@ -118,11 +174,5 @@ export class JsxElement<P> {
             value.subscribe(v => this.setAttribute(prop, v))
         }
         this.element!.setAttribute(prop, v)
-    }
-
-    private deleteChildren(): void {
-        while (this.element?.firstChild) {
-            this.element.removeChild(this.element.firstChild)
-        }
     }
 }
